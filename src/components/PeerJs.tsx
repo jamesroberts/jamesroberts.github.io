33bac1a8352b10c4df6e4124ebcba10c088e550b
James Roberts <jamesroberts.dev@gmail.com>
2021-01-24 20:23:42 +0200

    Clean up and group video functionality

--- src/components/PeerJs.tsx
+++ src/components/PeerJs.tsx
@@ -1,120 +1,168 @@
-import Peer from 'peerjs';
+import Peer, { DataConnection, MediaConnection } from 'peerjs';
 import React, { useState, useEffect } from 'react'
 import { Button, Input } from '@material-ui/core';
 import VideoChat from "./VideoChat";
-import ReactPlayer from 'react-player'
+import { Switch } from '@material-ui/core/';
+import VideoCallIcon from '@material-ui/icons/VideoCall';
 
 export default function PeerJs() {
 
     const [id, setId] = useState("");
     const [peer, setPeer] = useState(new Peer());
     const [friendId, setFriendId] = useState('');
-    const [conn, setConn] = useState<any>(null);
-    const [peerStream, setPeerStream] = useState<any>(null);
-    const [myStream, setStream] = useState<any>(null);
+    const [dataConnection, setDataConnection] = useState<DataConnection | null>(null);
+    const [mediaConnection, setMediaConnection] = useState<MediaConnection | null>(null);
+    const [peerStreams, setPeerStreams] = useState<MediaStream[] | []>([]);
+    const [userStream, setUserStream] = useState<MediaStream | null>(null);
+    const [shareScreen, setShareScreen] = useState<boolean>(false);
 
-    useEffect(() => {
-        peer.on('open', function (id: any) {
-            setId(id)
-        });
+    function handleNewPeer(id: string) {
+        setId(id)
+    }
 
-        peer.on('connection', function (conn: any) {
-            conn.on('data', function (data: any) {
-                console.log(data);
-            });
-            conn.on('open', () => {
-                console.log("Connection opened");
-                console.log("Connected to :" + conn.peer);
-            });
-            setConn(conn);
-            setFriendId(conn.peer);
-        });
-        peer.on('call', function (call) {
-            // Answer the call, providing our mediaStream
-            setFriendId(call.peer);
-            navigator.mediaDevices.getUserMedia({ audio: false, video: true })
-                .then(function (mediaStream) {
-                    console.log("Answering media stream..");
-                    console.log(mediaStream);
-                    call.answer(mediaStream);
-                    setStream(mediaStream);
-                    call.on('stream', function (stream) {
-                        console.log("Setting stream from caller");
-                        setPeerStream(stream);
-                    });
-                })
-                .catch(function (err) {
-                    console.log(err);
-                });
-        });
+    function handlePeerDataConnection(conn: DataConnection) {
+        conn = addDataConnectionListeners(conn);
+        setDataConnection(conn);
+        setFriendId(conn.peer);
+    }
 
-        console.log("Peer initialized");
-    }, [peer]);
+    function getAvailableConstraints() {
+        // TODO: Get specific constraints
+        return { audio: false, video: true };
+    }
 
-    useEffect(() => {
-        console.log("Streams updated");
-        console.log(myStream);
-        console.log(peerStream);
-    }, [myStream, peerStream])
+    async function handlePeerRecieveMediaCall(call: MediaConnection) {
+        // TODO: Add an answer or decline prompt
+        setFriendId(call.peer);
+        let constraints = getAvailableConstraints();
+        try {
+            let stream;
+            if (shareScreen) {
+                // @ts-ignore
+                stream = await navigator.mediaDevices.getDisplayMedia(constraints);
+            } else {
+                stream = await navigator.mediaDevices.getUserMedia(constraints);
+            }
 
-    function send() {
-        if (conn) {
-            conn.send('Hello!');
+            call.answer(stream);
+            setUserStream(stream);
+            call = addMediaConectionListeners(call);
+            setMediaConnection(call);
+        } catch (error) {
+            console.log(error);
         }
     }
 
-    function connect() {
-        console.log("Connecting...");
-        let conn = peer.connect(friendId);
-        conn.on('data', function (data: any) {
+    function addDataConnectionListeners(conn: DataConnection) {
+        conn.on('open', () => {
+            console.log("Connected opened with peer: " + conn.peer);
+        });
+        conn.on('data', (data: any) => {
+            // TODO: Set this to state.
             console.log(data);
         });
-        setConn(conn);
+        conn.on('close', () => {
+            // Handle error
+            console.log("Connection closed.");
+        });
+        conn.on('error', (error: any) => {
+            // Handle error
+            console.log(error);
+        });
+        return conn;
     }
 
-    function call() {
-        console.log("Calling...");
-        navigator.mediaDevices.getUserMedia({ audio: false, video: true })
-            .then(function (stream) {
-                setStream(stream);
-                let call = peer.call(friendId, stream);
-                call.on('stream', function (stream) {
-                    console.log("Setting stream from peer");
-                    setPeerStream(stream);
-                });
-                setConn(call);
-            })
-            .catch(function (err) {
-                console.log(err);
-            });
+    function addMediaConectionListeners(call: MediaConnection) {
+        call.on('stream', (stream) => setPeerStreams(peerStreams => [...peerStreams, stream]));
+        call.on('error', (error: any) => console.log(error));
+        call.on('close', function () {
+            setPeerStreams(() => []);
+            setMediaConnection(null);
+        });
+        return call;
     }
+
+    function stopAllStreams() {
+        // @ts-ignore
+        userStream?.getTracks().forEach(track => track.stop());
+        peerStreams?.forEach((stream: MediaStream) => stream.getTracks().forEach(track => track.stop()));
+        setUserStream(null);
+        setPeerStreams(() => []);
+    }
+
+    useEffect(() => {
+        peer.on('open', handleNewPeer);
+        peer.on('connection', handlePeerDataConnection);
+        peer.on('call', handlePeerRecieveMediaCall);
+        console.log("Peer initialized");
+    }, [peer]);
+
+    function send(conn: DataConnection) {
+        conn.send('Hello!');
+    }
+
+    function connect() {
+        let conn = peer.connect(friendId);
+        conn = addDataConnectionListeners(conn);
+        setDataConnection(conn);
+    }
+
+    function disconnectMediaConnection() {
+        mediaConnection?.close()
+        setMediaConnection(null);
+        stopAllStreams();
+    }
+
+    async function call() {
+        try {
+            let stream;
+            if (shareScreen) {
+                // @ts-ignore
+                stream = await navigator.mediaDevices.getDisplayMedia({ audio: false, video: true });
+            } else {
+                stream = await navigator.mediaDevices.getUserMedia({ audio: false, video: true });
+            }
+
+            let call = peer.call(friendId, stream);
+            call = addMediaConectionListeners(call);
+            setMediaConnection(call);
+            setUserStream(stream);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const handleSwitchChange = (event: any) => {
+        setShareScreen(event.target.checked)
+    };
 
     return (
         <div>
             <Input onChange={event => setFriendId(event.target.value)} />
             {/* <Button variant="contained" color="primary" onClick={send}> Send </Button>
             <Button variant="contained" color="primary" onClick={connect}> Conect </Button> */}
-            <Button variant="contained" color="primary" onClick={call}> Call </Button>
-
+            <Button variant="contained" color="primary" onClick={call}> <VideoCallIcon fontSize="large" /> </Button>
+            <Switch
+                checked={shareScreen}
+                onChange={handleSwitchChange}
+                color="primary"
+                name="checkedB"
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+            /> Share Screen
             <div>
                 {"MY ID: " + id}
                 <br />
                 {"Connecting to: " + friendId}
             </div>
-            <div style={{ position: "relative", top: 0, right: 0, marginBottom: 250, marginTop: 25 }} >
-                <div>
-                    <ReactPlayer url={peerStream} playing={true} width="100%" height="100%" />
-                </div>
-                <div style={{ position: "absolute", top: 0, right: 0 }}>
-                    <ReactPlayer url={myStream} playing={true} width="25%" height="25%" />
-                </div>
-            </div>
+            <VideoChat peerStreams={peerStreams} userStream={userStream} />
+            <div>
+                {
+                    mediaConnection
+                        ? <Button variant="contained" color="secondary" onClick={disconnectMediaConnection}> Disconnect </Button>
+                        : null
+                }
 
+            </div>
         </div >
     )
 }
-
-
-
-
-
--- src/components/VideoChat.tsx
+++ src/components/VideoChat.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import ReactPlayer from 'react-player'
 
-export default function VideoChat(myStream: any, peerStream: any) {
+export default function VideoChat(props: any) {
+    console.log(props.peerStreams);
+    let width = "100%";
+    let height = "100%";
+    if (props.peerStreams.length > 0) {
+        width = `${100 / props.peerStreams.length}%`
+        height = `${100 / props.peerStreams.length}%`
+    }
+
     return (
-        <div>
-            <ReactPlayer url={peerStream} playing={true}>
-                <ReactPlayer url={myStream} playing={true} width={25} height={50} />
-            </ReactPlayer>
+        <div style={{ position: "relative", top: 0, right: 0, marginBottom: 25, marginTop: 25 }} >
+            <ul>
+                {props.peerStreams?.map((stream: MediaStream) => (
+                    <li key={stream.id}>
+                        <ReactPlayer url={stream} playing={true} width={width} height={height} />
+                    </li>
+                ))}
+            </ul>
+            <div style={{ position: "absolute", top: 0, right: 0 }}>
+                <ReactPlayer url={props.userStream} playing={true} width="25%" height="25%" />
+            </div>
         </div>
     )
 }
